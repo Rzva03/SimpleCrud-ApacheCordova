2c8ab239f210727e8049e25d235339b4d1296583
17920368 <avictor.zrodriguez@gmail.com>
2021-12-18 17:06:20 -0600

    script

--- www/js/index.js
+++ www/js/index.js
@@ -1,15 +1,40 @@
 document.addEventListener("deviceready", onDeviceReady, false);
 
 function onDeviceReady() {
-  // Cordova is now initialized. Have fun!
-
   console.log("Running cordova-" + cordova.platformId + "@" + cordova.version);
   document.getElementById("deviceready").classList.add("ready");
 }
 const form = document.getElementById("studentForm");
+let ArrayStudent = [];
 form.addEventListener("submit", (e) => {
   e.preventDefault();
-  let formData = new FormData(form); //crea el form data mediante el formulario
-  saveToArray(formData);
-  form.reset(); //formatea el formulario
+  let formData = new FormData(form);
+  if (formData.get("select-carreras") == "0") {
+    alert("Complete los campos");
+  } else {
+    let studentObj = convertToObjet(formData);
+    saveToArray(studentObj);
+    form.reset();
+  }
 });
+
+function convertToObjet(formData) {
+  let control = formData.get("control");
+  let name = formData.get("name");
+  let carrera = formData.get("select-carreras");
+  let semestre = formData.get("semestre");
+  let grupo = formData.get("grupo");
+
+  return {
+    control: control,
+    name: name,
+    carrera: carrera,
+    semestre: semestre,
+    grupo: grupo,
+  };
+}
+
+function saveToArray(studentObj) {
+  ArrayStudent.push(studentObj);
+  console.log(ArrayStudent);
+}
