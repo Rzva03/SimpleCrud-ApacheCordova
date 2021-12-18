e2c38f18558977b350a04fe8634a84ebe1ced079
17920368 <avictor.zrodriguez@gmail.com>
2021-12-18 16:45:28 -0600

    TODO

--- www/js/index.js
+++ www/js/index.js
@@ -6,18 +6,10 @@ function onDeviceReady() {
   console.log("Running cordova-" + cordova.platformId + "@" + cordova.version);
   document.getElementById("deviceready").classList.add("ready");
 }
-const firebaseConfig = {
-  apiKey: "AIzaSyBWKUojeZPeALweilAOIheNqgyrQrYGU8o",
-  authDomain: "crud-cf57a.firebaseapp.com",
-  projectId: "crud-cf57a",
-  storageBucket: "crud-cf57a.appspot.com",
-  messagingSenderId: "415923140860",
-  appId: "1:415923140860:web:ac5244a4b4dc13564d7f79",
-};
-const app = initializeApp(firebaseConfig);
 const form = document.getElementById("studentForm");
 form.addEventListener("submit", (e) => {
   e.preventDefault();
-  const nombre = form["name"].value;
-  console.log(nombre);
+  let formData = new FormData(form); //crea el form data mediante el formulario
+  saveToArray(formData);
+  form.reset(); //formatea el formulario
 });
