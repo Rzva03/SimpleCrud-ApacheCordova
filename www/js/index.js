d8d9787f54df3fe32ff9447a4356ff2bbc4b4599
17920368 <avictor.zrodriguez@gmail.com>
2021-12-18 15:10:54 -0600

    views

--- www/js/index.js
+++ www/js/index.js
@@ -1,29 +1,23 @@
-/*
- * Licensed to the Apache Software Foundation (ASF) under one
- * or more contributor license agreements.  See the NOTICE file
- * distributed with this work for additional information
- * regarding copyright ownership.  The ASF licenses this file
- * to you under the Apache License, Version 2.0 (the
- * "License"); you may not use this file except in compliance
- * with the License.  You may obtain a copy of the License at
- *
- * http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing,
- * software distributed under the License is distributed on an
- * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
- * KIND, either express or implied.  See the License for the
- * specific language governing permissions and limitations
- * under the License.
- */
-
-// Wait for the deviceready event before using any of Cordova's device APIs.
-// See https://cordova.apache.org/docs/en/latest/cordova/events/events.html#deviceready
-document.addEventListener('deviceready', onDeviceReady, false);
+document.addEventListener("deviceready", onDeviceReady, false);
 
 function onDeviceReady() {
-    // Cordova is now initialized. Have fun!
+  // Cordova is now initialized. Have fun!
 
-    console.log('Running cordova-' + cordova.platformId + '@' + cordova.version);
-    document.getElementById('deviceready').classList.add('ready');
+  console.log("Running cordova-" + cordova.platformId + "@" + cordova.version);
+  document.getElementById("deviceready").classList.add("ready");
 }
+const firebaseConfig = {
+  apiKey: "AIzaSyBWKUojeZPeALweilAOIheNqgyrQrYGU8o",
+  authDomain: "crud-cf57a.firebaseapp.com",
+  projectId: "crud-cf57a",
+  storageBucket: "crud-cf57a.appspot.com",
+  messagingSenderId: "415923140860",
+  appId: "1:415923140860:web:ac5244a4b4dc13564d7f79",
+};
+const app = initializeApp(firebaseConfig);
+const form = document.getElementById("studentForm");
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+  const nombre = form["name"].value;
+  console.log(nombre);
+});
